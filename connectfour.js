f959d7ee838a1f22e15fbb07137f37237a20686b
Travis Anderson <thetanderson@gmail.com>
2018-05-09 15:40:19 -0400

    added a mouse follower astro boy, makes game glitchy, but is neat feature

--- connectfour.js
+++ connectfour.js
@@ -1,7 +1,6 @@
-let testjs = document.getElementById("test")
-testjs.style.border = "solid 1pt black"
-turn = "red"
-winner = ""
+let turn = "red"
+let winner = ""
+let gameover = ""
 const displayTurn = document.getElementById("displayTurn")
 const columns = document.getElementsByClassName("column");
 let wrapper = document.getElementsByClassName("wrapper");
@@ -30,6 +29,7 @@ function dropDisc(event) {
     // } else {
         let arrayNumber = parseInt(clickedColumn.id)
         console.log(arrayNumber)
+        
         if (clickedColumn.childElementCount >= 6) {
             alert("There are already 6 in this column!")
         } else {
@@ -65,19 +65,20 @@ function dropDisc(event) {
         console.log(board)
 
 
+
+        if(gameover === ""){
         const edgeX = board[0].length - 3;
         const edgeY = board.length - 3;
         //display winner on screen is up and down
         for (let columnIndex = 0; columnIndex < board.length; columnIndex++) {
-            for (let rowIndex = 0; rowIndex < edgeX; rowIndex++) {
+            for (let rowIndex = 0; rowIndex < board[0].length; rowIndex++) {
                 let cell = board[columnIndex][rowIndex];
                 if (cell !== 0) {
                     if (cell === board[columnIndex][rowIndex + 1] &&
                         cell === board[columnIndex][rowIndex + 2] &&
                         cell === board[columnIndex][rowIndex + 3]) {
-                       
                         document.getElementById("winner").textContent = (winner + "'s a winner up. Click here to Reset");
-                        
+                        gameover = winner
                         
                     }
                 }
@@ -86,14 +87,14 @@ function dropDisc(event) {
         }
         //display winner on screen is row-across
         for (let columnIndex = 0; columnIndex < board.length - 3; columnIndex++) {
-            for (let rowIndex = 0; rowIndex < edgeX; rowIndex++) {
+            for (let rowIndex = 0; rowIndex < board[0].length; rowIndex++) {
                 let cell = board[columnIndex][rowIndex];
                 if (cell !== 0) {
                     if (cell === board[columnIndex + 1][rowIndex] &&
                         cell === board[columnIndex + 2][rowIndex] &&
                         cell === board[columnIndex + 3][rowIndex]) {
                         document.getElementById("winner").textContent = (winner + "'s a winner across. Click here to Reset")
-                        
+                        gameover = winner
                     }
                 }
 
@@ -107,9 +108,8 @@ function dropDisc(event) {
                     if (cell === board[columnIndex + 1][rowIndex + 1] &&
                         cell === board[columnIndex + 2][rowIndex + 2] &&
                         cell === board[columnIndex + 3][rowIndex + 3]) {
-
                         document.getElementById("winner").textContent = (winner + "'s a upright Diagonal. Click here to Reset")
-                        
+                        gameover = winner
                     }
                 }
 
@@ -124,7 +124,7 @@ function dropDisc(event) {
                         cell === board[columnIndex - 2][rowIndex + 2] &&
                         cell === board[columnIndex - 3][rowIndex + 3]) {
                         document.getElementById("winner").textContent = (winner + "'s down left Diagonal. Click here to Reset");
-                       
+                        gameover = winner
                     }
                 }
 
@@ -132,12 +132,15 @@ function dropDisc(event) {
         }
 
     }
+}
+
 
     document.getElementById("winner").onclick = function(){
         document.getElementById("winner").innerHTML = "Click Here to Reset"
-        for(let i=0;i<6;i++){
+        for(let i=0;i< 7 ;i++){
             let resetColumn =  document.getElementById(i);
             resetColumn.innerHTML = ""
+            gameover=""
          }
          let board = [
             [0, 0, 0, 0, 0, 0],
@@ -151,6 +154,51 @@ function dropDisc(event) {
         ]
     }
     
+    //astro boy animation below
+    var bee = document.getElementById("bee");
+    document.addEventListener("mousemove", getMouse); 
+
+
+    bee.style.position = "absolute"; //css		
+    var beepos = {x:0, y:0};
+
+    setInterval(followMouse, 50);
+    
+    var mouse = {x:0, y:0}; //mouse.x, mouse.y
+    
+    var dir = "right";
+    function getMouse(e){
+        mouse.x = e.pageX;
+        mouse.y = e.pageY;
+    //Checking directional change
+    if(mouse.x > beepos.x){
+      dir = "right";
+    } else {
+      dir = "left";
+    }
+    }
+    
+    function followMouse(){
+        //1. find distance X , distance Y
+        var distX = mouse.x - beepos.x;
+        var distY = mouse.y - beepos.y;
+        //Easing motion
+   //Progressive reduction of distance 
+        beepos.x += distX/5;
+        beepos.y += distY/2;
+        
+        bee.style.left = beepos.x + "px";
+        bee.style.top = beepos.y + "px";
+  
+  
+    //Apply css class 
+    if (dir == "right"){
+      bee.setAttribute("class", "right");
+    } else {
+      bee.setAttribute("class", "left");        
+    }
+        
+    }
         
     
 
