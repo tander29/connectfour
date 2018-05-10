3fbc2d5a1ad11f4e51bc7bf8d527afb74c81eacd
Travis Anderson <thetanderson@gmail.com>
2018-05-10 10:03:14 -0400

    declared same variable twice caused error during reset, fixed

--- connectfour.js
+++ connectfour.js
@@ -6,11 +6,11 @@ const columns = document.getElementsByClassName("column");
 let wrapper = document.getElementsByClassName("wrapper");
 
 let board = [
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
+    ["", 0, 0, 0, 0, 0],
+    ["", 0, 0, 0, 0, 0],
+    ["", 0, 0, 0, 0, 0],
+    ["", 0, 0, 0, 0, 0],
+    ["", 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
 
@@ -140,9 +140,12 @@ function dropDisc(event) {
         for(let i=0;i< 7 ;i++){
             let resetColumn =  document.getElementById(i);
             resetColumn.innerHTML = ""
-            gameover=""
+            gameover = ""
+         
+        
          }
-         let board = [
+         winner= ""
+         board = [
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
@@ -154,51 +157,51 @@ function dropDisc(event) {
         ]
     }
     
-    //astro boy animation below
-    var bee = document.getElementById("bee");
-    document.addEventListener("mousemove", getMouse); 
+//     //astro boy animation below
+//     var bee = document.getElementById("bee");
+//     document.addEventListener("mousemove", getMouse); 
 
 
-    bee.style.position = "absolute"; //css		
-    var beepos = {x:0, y:0};
+//     bee.style.position = "absolute"; //css		
+//     var beepos = {x:0, y:0};
 
-    setInterval(followMouse, 50);
+//     setInterval(followMouse, 50);
     
-    var mouse = {x:0, y:0}; //mouse.x, mouse.y
+//     var mouse = {x:0, y:0}; //mouse.x, mouse.y
     
-    var dir = "right";
-    function getMouse(e){
-        mouse.x = e.pageX;
-        mouse.y = e.pageY;
-    //Checking directional change
-    if(mouse.x > beepos.x){
-      dir = "right";
-    } else {
-      dir = "left";
-    }
-    }
+//     var dir = "right";
+//     function getMouse(e){
+//         mouse.x = e.pageX;
+//         mouse.y = e.pageY;
+//     //Checking directional change
+//     if(mouse.x > beepos.x){
+//       dir = "right";
+//     } else {
+//       dir = "left";
+//     }
+//     }
     
-    function followMouse(){
-        //1. find distance X , distance Y
-        var distX = mouse.x - beepos.x;
-        var distY = mouse.y - beepos.y;
-        //Easing motion
-   //Progressive reduction of distance 
-        beepos.x += distX/5;
-        beepos.y += distY/2;
+//     function followMouse(){
+//         //1. find distance X , distance Y
+//         var distX = mouse.x - beepos.x;
+//         var distY = mouse.y - beepos.y;
+//         //Easing motion
+//    //Progressive reduction of distance 
+//         beepos.x += distX/5;
+//         beepos.y += distY/2;
         
-        bee.style.left = beepos.x + "px";
-        bee.style.top = beepos.y + "px";
+//         bee.style.left = beepos.x + "px";
+//         bee.style.top = beepos.y + "px";
   
   
-    //Apply css class 
-    if (dir == "right"){
-      bee.setAttribute("class", "right");
-    } else {
-      bee.setAttribute("class", "left");        
-    }
+//     //Apply css class 
+//     if (dir == "right"){
+//       bee.setAttribute("class", "right");
+//     } else {
+//       bee.setAttribute("class", "left");        
+//     }
         
-    }
+//     }
         
     
 
